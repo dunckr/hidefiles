ac61b25de106d6aa34135fbbde0c31493252fbfe
duncky <d@duncky.com>
2013-10-12 16:25:07 +0100

    using class

--- hidefiles.py
+++ hidefiles.py
@@ -1,116 +1,46 @@
 import sublime, sublime_plugin
 
-# class Files():
-
-#   def __init__(self,file_type):
-#     self.file_type = file_type.split('.').pop()
-#     self.setExlcusions()
-#     self.getPreferences()
-
-#   def setExlcusions(self):
-#     custom_settings = sublime.load_settings("hidefiles.sublime-settings")
-#     self.exclusions = custom_settings.get(self.file_type)
-
-#   def getPreferences(self):
-#     settings = sublime.load_settings("Preferences.sublime-settings")
-#     self.preferences = settings.get("file_exclude_patterns")
-
-#   def append(self):
-#     if exclusions is not None:
-#       for pattern in patterns:
-#         if pattern not in self.preferences:
-#           self.exclusions.append(pattern)
-#           # self.remove(patterns)
-#     preferences.set("file_exclude_patterns", zzzzzz!!!)
-#     sublime.save_settings("Preferences.sublime-settings")
-
-    # file_type = self.view.file_name().split('.').pop()
-    # hidefiles = sublime.load_settings("hidefiles.sublime-settings")
-    # exclusions = hidefiles.get(file_type)
-    # if exclusions is not None:
-    #   settings = sublime.load_settings("Preferences.sublime-settings")
-    #   current_list = settings.get("file_exclude_patterns")
-    #   for exclude in exclusions:
-    #     if exclude not in current_list:
-    #       current_list.append(exclude)
-    #   settings.set("file_exclude_patterns", current_list)
-    #   sublime.save_settings("Preferences.sublime-settings")
-
-
-  # def remove(self, patterns):
-  #   print 'refactor'
-
-  # def run(self):
-  # def load(self):
-  #   exclusions = hidefiles.get(file_type)
-  #   custom_settings = sublime.load_settings("hidefiles.sublime-settings")
-
-
-class Files():
+class Exclusions():
 
   def __init__(self,file_name):
     self.file_type = file_name.split('.').pop()
-    self.setExlcusions()
+    self.setExclusions()
+    self.getSettings()
 
-  def setExlcusions(self):
-    hidefiles = sublime.load_settings("hidefiles.sublime-settings")
-    self.exclusions = hidefiles.get(self.file_type)
+  def setExclusions(self):
+    custom_settings = sublime.load_settings("hidefiles.sublime-settings")
+    self.exclusions = custom_settings.get(self.file_type)
+
+  def getSettings(self):
+    self.settings = sublime.load_settings("Preferences.sublime-settings")
+    self.patterns = self.settings.get("file_exclude_patterns")
+
+  def saveSettings(self):
+    self.settings.set("file_exclude_patterns", self.patterns)
+    sublime.save_settings("Preferences.sublime-settings")
 
   def hide(self):
     if self.exclusions is not None:
-      settings = sublime.load_settings("Preferences.sublime-settings")
-      current_list = settings.get("file_exclude_patterns")
       for exclude in self.exclusions:
-        if exclude not in current_list:
-          current_list.append(exclude)
-      settings.set("file_exclude_patterns", current_list)
-      sublime.save_settings("Preferences.sublime-settings")
+        if exclude not in self.patterns:
+          self.patterns.append(exclude)
+      self.saveSettings()
 
   def show(self):
     if self.exclusions is not None:
-      settings = sublime.load_settings("Preferences.sublime-settings")
-      current_list = settings.get("file_exclude_patterns")
       for exclude in self.exclusions:
-        current_list.remove(exclude)
-      settings.set("file_exclude_patterns", current_list)
-      sublime.save_settings("Preferences.sublime-settings")
+        self.patterns.remove(exclude)
+      self.saveSettings()
 
 
 class HideFilesCommand(sublime_plugin.TextCommand):
   def run(self,edit):
-    print 'hide files'
-
     file_name = self.view.file_name()
-    f = Files(file_name)
+    f = Exclusions(file_name)
     f.hide()
-
-    # file_type = self.view.file_name().split('.').pop()
-    # hidefiles = sublime.load_settings("hidefiles.sublime-settings")
-    # exclusions = hidefiles.get(file_type)
-    # if exclusions is not None:
-    #   settings = sublime.load_settings("Preferences.sublime-settings")
-    #   current_list = settings.get("file_exclude_patterns")
-    #   for exclude in exclusions:
-    #     if exclude not in current_list:
-    #       current_list.append(exclude)
-    #   settings.set("file_exclude_patterns", current_list)
-    #   sublime.save_settings("Preferences.sublime-settings")
-
 
 class ShowFilesCommand(sublime_plugin.TextCommand):
   def run(self,edit):
-    print 'show files'
     file_name = self.view.file_name()
-    f = Files(file_name)
+    f = Exclusions(file_name)
     f.show()
-
-    # file_type = self.view.file_name().split('.').pop()
-    # hidefiles = sublime.load_settings("hidefiles.sublime-settings")
-    # exclusions = hidefiles.get(file_type)
-    # if exclusions is not None:
-    #   settings = sublime.load_settings("Preferences.sublime-settings")
-    #   current_list = settings.get("file_exclude_patterns")
-    #   for exclude in exclusions:
-    #     current_list.remove(exclude)
-    #   settings.set("file_exclude_patterns", current_list)
-    #   sublime.save_settings("Preferences.sublime-settings")
